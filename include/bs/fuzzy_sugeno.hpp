959d071dc8fe326ea422072f9497fa1b5c4103ff
Liviu Nicoara <nikkoara at hates dot ms>
2017-06-27 14:08:44 -0400

    Added citation.

--- include/bs/fuzzy_sugeno.hpp
+++ include/bs/fuzzy_sugeno.hpp
@@ -8,6 +8,25 @@
 
 namespace bs {
 
+//
+// @inproceedings{zhang:2006:FCT:2092283.2092396,
+//  author = {zhang, Hongxun and xu, De},
+//  title = {Fusing Color and Texture Features for Background Model},
+//  booktitle = {Proceedings of the Third International Conference on Fuzzy Systems and Knowledge Discovery},
+//  series = {FSKD'06},
+//  year = {2006},
+//  isbn = {3-540-45916-2, 978-3-540-45916-3},
+//  location = {Xi'an, China},
+//  pages = {887--893},
+//  numpages = {7},
+//  url = {http://dx.doi.org/10.1007/11881599_110},
+//  doi = {10.1007/11881599_110},
+//  acmid = {2092396},
+//  publisher = {Springer-Verlag},
+//  address = {Berlin, Heidelberg},
+// }
+//
+
 struct fuzzy_sugeno_bootstrap {
     explicit fuzzy_sugeno_bootstrap (double = .1, size_t = 10UL);
 
